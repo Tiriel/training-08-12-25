fb33fbdd68e5b1e14c48542438b940b8664231e5
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2025-12-10 12:20:47 +0100

    Repositories

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,16 +3,40 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route('/conference')]
 final class ConferenceController extends AbstractController
 {
+    #[Route('', name: 'app_conference_list', methods: ['GET'])]
+    public function list(ConferenceRepository $repository, #[MapQueryParameter] int $page = 1): Response
+    {
+        $conferences = \array_map(
+            fn(Conference $c) => ['id' => $c->getId(), 'name' => $c->getName()],
+            $repository->findBy([], [], 20 , 20 * ($page -1))
+        );
+
+        return $this->json($conferences);
+    }
+
+    #[Route('/{id}', name: 'app_conference_show', methods: ['GET'])]
+    public function show(Conference $conference)
+    {
+        return $this->json([
+            'id' => $conference->getId(),
+            'name' => $conference->getName(),
+        ]);
+    }
+
     #[Route(
-        '/conference/{name}/{start}/{end}',
+        '/{name}/{start}/{end}',
         name: 'app_conference_new',
         requirements: ['name' => '[a-zA-Z ]+', 'start' => Requirement::DATE_YMD, 'end' => Requirement::DATE_YMD],
     )]
--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Conference;
+use App\Exception\MissingDatesExceptions;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,6 +15,30 @@ class ConferenceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Conference::class);
+    }
+
+    /**
+     * @return Conference[]
+     */
+    public function findConferencesBetweenDates(?\DateTimeImmutable $start = null, ?\DateTimeImmutable $end = null): array
+    {
+        if (null === $start && null === $end) {
+            throw new MissingDatesExceptions(sprintf("At least one date must be passed to %s", __METHOD__));
+        }
+
+        $qb = $this->createQueryBuilder('c');
+
+        if ($start instanceof \DateTimeInterface) {
+            $qb->andWhere('c.startAt >= :start')
+                ->setParameter('start', $start);
+        }
+
+        if ($end instanceof \DateTimeInterface) {
+            $qb->andWhere($qb->expr()->lte('c.endAt', ':end'))
+                ->setParameter('end', $end);
+        }
+
+        return $qb->getQuery()->getResult();
     }
 
     //    /**
