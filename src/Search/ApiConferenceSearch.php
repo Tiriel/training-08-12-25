ee0bf5181f535cf6027a6800cb369858918ed08a
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2025-12-11 14:33:35 +0100

    HttpClient injection

--- src/Search/ApiConferenceSearch.php
+++ src/Search/ApiConferenceSearch.php
@@ -2,18 +2,26 @@
 
 namespace App\Search;
 
-use App\Search\ConferenceSearchInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ApiConferenceSearch implements ConferenceSearchInterface
 {
     public function __construct(
+        private readonly HttpClientInterface $client,
         #[Autowire(env: 'CONF_API_KEY')]
         private string $apiKey,
     ){}
 
     public function searchByName(?string $name = null): array
     {
-        // TODO: Implement searchByName() method.
+        return $this->client->request(Request::METHOD_GET, 'https://www.devevents-api.fr/events', [
+            'query' => ['name' => $name],
+            'headers' => [
+                'apikey' => $this->apiKey,
+                'Accept' => 'application/json',
+            ],
+        ])->toArray();
     }
 }
